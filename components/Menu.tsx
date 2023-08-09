96efbd6a5026d33f57e513e9518715a0978d8ab6
jai <jai.bhalla2021@vitstudent.ac.in>
2023-08-09 11:55:43 +0530

    adding the menu items slider

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import MenuCards from "@/components/MenuCards";
 
 const Menu = () => {
     return (
-        <div className={'h-screen bg-[#eee1d1]'}>
-
+        <div className={'bg-[#eee1d1]'}>
+            <div className={'mx-48 text-center pt-20 text-5xl flex flex-col items-center justify-center font-semibold text-[#285f58]'}>
+                Menu that our customers<br/>
+                <div className={'relative'}>
+                    Loved the most.
+                    <svg className={'absolute'} width="380" height="17" viewBox="0 0 477 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 16C53.6667 8.59354 190.2 -4.30682 315 3.34339C439.8 10.9936 474.333 14.9687 476 16" stroke="#e85731" stroke-width={"3"}/>
+                    </svg>
+                </div>
+                <div className={'text-sm mt-10 mx-48'}>
+                    Our food is made of the freshest ingredients and the result is a much better sushi with a fresh look and of course great taste guarantee.
+                </div>
+            </div>
+            <MenuCards/>
         </div>
     );
 };
---
+++ components/MenuCards.tsx
@@ -0,0 +1,94 @@
+'use client';
+import React, {useState} from 'react';
+import image1 from '../images/pizzaImage.png'
+import image2 from '../images/burgerImage.png'
+import image3 from '../images/saladImage.png'
+import Image from "next/image";
+import {AiOutlineLeft, AiOutlineRight} from "react-icons/ai";
+const MenuCards = () => {
+    let [activeCard, setActiveCard] = useState(0);
+
+    const menuItem = [
+        {
+            imageSrc: image1,
+            title: 'Pizza',
+            description: 'A classic Italian delight with various toppings.'
+        },
+        {
+            imageSrc: image2,
+            title: 'Burger',
+            description: 'A juicy burger with all the fixings.'
+        },
+        {
+            imageSrc: image3,
+            title: 'Salad',
+            description: 'Fresh and healthy greens with dressing.'
+        },
+    ]
+
+    const handleClick = (cardIndex) =>{
+        setActiveCard(cardIndex);
+    }
+    const shiftRight = () => {
+        const len = menuItem.length;
+        let newLen = (activeCard + 1) % len; // Increment correctly
+        setActiveCard(newLen);
+    };
+
+    const shiftLeft = () => {
+        const len = menuItem.length;
+        let newLen = (activeCard - 1 + len) % len; // Handle negative value correctly
+        setActiveCard(newLen);
+    };
+    return (
+        <div className={'h-screen pt-20 px-32'}>
+            <div className={'flex justify-between px-20'}>
+                <div className={'relative'}>
+                    {
+                        menuItem.map((item, index)=>(
+                            <div key={index}
+                                 className={`${activeCard === index ? 'opacity-1 scale-100' : 'opacity-0 scale-50'} text-[#285f58] flex flex-col items-center justify-center gap-4 transform duration-500 h-96 w-96 absolute`}
+                            >
+                                <Image src={item.imageSrc} alt={item.title}/>
+                                <div className={'text-xl font-bold'}>{item.title}</div>
+                                <div className={'text-sm font-semibold'}>{item.description}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className={'flex flex-col gap-4 text-[#285f58]'}>
+                    {
+                        menuItem.map((item, index)=>(
+                            <div onClick={()=>handleClick(index)} className={`group hover:bg-[#e8512a] hover:text-white transform duration-300 cursor-pointer rounded-l-full ${activeCard === index ? 'bg-[#e8512a] text-white' : 'bg-[#d6c7b6] text-[#285f58]'}  w-full flex gap-2 p-1`}>
+                                <div className={'flex items-center justify-center p-2 h-20 max-fit rounded-full'}>
+                                    <Image src={item.imageSrc} alt={item.title} className={'w-24 rounded-full'}/>
+                                </div>
+                                <div className={'px-1 flex w-full flex-col gap-2 justify-center'}>
+                                    <div className={'items-center flex w-full justify-between'}>
+                                        <div className={'text-lg font-bold'}>
+                                            {item.title}
+                                        </div>
+                                        <div className={`group-hover:bg-white group-hover:text-[#e8512a] group-hover:font-semibold transform duration-300 w-28 flex items-center justify-center p-1 shadow-md shadow-orange-800 cursor-pointer ${activeCard === index ? 'font-semibold text-[#e8512a] bg-white' : 'font-thin text-white bg-[#e8512a]'} text-sm rounded-bl-xl rounded-tr-xl rounded-tl-sm rounded-br-sm`}>
+                                            Order Now
+                                        </div>
+                                    </div>
+                                    <div>{item.description}</div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    <div className={'flex w-full justify-center gap-4 pt-6'}>
+                        <div className={'p-2 bg-[#d6c7b6] text-[#e8512a] hover:bg-[#e8512a] hover:text-white transform duration-300 cursor-pointer rounded-full'} onClick={shiftLeft}>
+                            <AiOutlineLeft size={25}/>
+                        </div>
+                        <div className={'p-2 bg-[#d6c7b6] text-[#e8512a] hover:bg-[#e8512a] hover:text-white transform duration-300 cursor-pointer rounded-full'} onClick={shiftRight}>
+                            <AiOutlineRight size={25}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCards;
